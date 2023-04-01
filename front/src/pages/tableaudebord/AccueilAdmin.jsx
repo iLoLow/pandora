ecc70b70f72649ff6546cfc4c13b455076587ecb
Laurent <soyezlaurent@gmail.com>
2023-04-01 22:39:16 +0200

    preview home admin

---
+++ front/src/pages/tableaudebord/AccueilAdmin.jsx
@@ -0,0 +1,13 @@
+import "../../styles/tableaudebord/AccueilAdmin.css"
+
+function AccueilAdmin() {
+  return (
+    <section className="accueilAdminWrapper">
+      {/* header */}
+      <div className="accueilAdminHeader">
+        <h2>Bienvenue sur le tableau de bord</h2>
+      </div>
+    </section>
+  );
+}
+export default AccueilAdmin;

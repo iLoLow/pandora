182b75853ea508fa82c172a4e12664cafc3805e0
Laurent <soyezlaurent@gmail.com>
2023-04-01 22:38:56 +0200

    add link home logout

--- front/src/components/ProfilMenu.jsx
+++ front/src/components/ProfilMenu.jsx
@@ -26,6 +26,7 @@ function ProfilMenu({ user }) {
             Tableau de bord
           </Link>
           <Link
+            to="/"
             onClick={() => {
               setIsOpen(!isOpen);
               dispatch(setLogout());

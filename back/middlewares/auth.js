41d1b9b1e518269fd5a23a1d4adab9c15c530d4b
Laurent <soyezlaurent@gmail.com>
2023-04-01 18:26:50 +0200

    refactor erreur, et next

--- back/middlewares/auth.js
+++ back/middlewares/auth.js
@@ -6,24 +6,25 @@ export const verifyToken = (req, res, next) => {
 
     // vérifie si le token est présent
     if (!token) {
-      return res.status(403).json({ message: "Vous n'êtes pas autorisé, veuillez vous connecter.", code: 403 });
+      return res.status(403).json({ error: "Vous n'êtes pas autorisé, veuillez vous connecter.", code: 403 });
     }
 
     if (token.startsWith("Bearer ")) {
       // Supprime le mot clé Bearer du token
       token = token.slice(7, token.length).trimLeft();
     }
-    // Vérifie si le token est valide et non expiré
 
-    jwt.verify(token, process.env.JWT_SECRET, { algorithms: ["HS256"] }, (err, decoded) => {
+    // Vérifie si le token est valide et non expiré
+    jwt.verify(token, process.env.JWT_SECRET_KEY, { algorithms: ["HS256"] }, (err, decoded) => {
       if (err) {
-        return res.status(403).json({ message: "Vous n'êtes pas autorisé, veuillez vous connecter.", code: 403 });
-        next();
+        return res.status(403).json({ error: "Vous n'êtes pas autorisé, veuillez vous connecter.", code: 403 });
       }
-      return (req.user = decoded);
+      req.user = decoded;
       next();
     });
+
   } catch (error) {
-    res.status(500).json({ message: "Autorisation échouée, veuillez vous connecter." });
+    res.status(500).json({ error: "Autorisation échouée, veuillez vous connecter." });
+    next();
   }
 };

84fffeac8574ef6f3cd75268bcb4e9ef8837690b
iLoLow <soyez.laurent.dev@gmail.com>
2023-04-12 23:30:43 +0200

    fix globalthis pour draftjs

--- front/vite.config.js
+++ front/vite.config.js
@@ -7,6 +7,17 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      },
+    },
     server: {
       proxy: {
         "/api": process.env.VITE_URL,
